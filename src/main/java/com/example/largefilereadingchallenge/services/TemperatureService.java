5027b0a4140a30c9175a41919fa9a266b9aeecf6
Andrzej Galus <redtiger84@users.noreply.github.com>
2023-10-29 18:41:09 +0100

    Temperature information - project challenge

--- src/main/java/com/example/largefilereadingchallenge/services/TemperatureService.java
+++ src/main/java/com/example/largefilereadingchallenge/services/TemperatureService.java
@@ -5,6 +5,9 @@ import com.example.largefilereadingchallenge.repository.TemperatureReader;
 import java.util.Map;
 import java.util.Set;
 import lombok.AllArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,8 +16,14 @@ public class TemperatureService {
 
     private final TemperatureReader temperatureReader;
 
+    @Cacheable(value = "cities")
     public Set<String> extractCities() throws DataFileException {
         return temperatureReader.extractCities();
+    }
+
+    @CacheEvict(value = "cities", allEntries = true)
+    @Scheduled(fixedRate = 300000)
+    public void clearCitiesCache() {
     }
 
 
